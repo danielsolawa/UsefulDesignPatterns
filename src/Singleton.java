a86e25ee8ba4b33b7284debcff720bee540f8cab
Daniel Solawa <danielsolawa@gmail.com>
2017-12-11 11:37:58 +0100

    added first singleton pattern

---
+++ src/Singleton.java
@@ -0,0 +1,20 @@
+/**
+ *
+ * This implementation is not thread safety!
+ */
+public class Singleton {
+
+    private static Singleton instance = null;
+
+    private Singleton(){
+
+    }
+
+    public static Singleton getInstance(){
+        if(instance == null){
+            instance = new Singleton();
+        }
+
+        return instance;
+    }
+}
