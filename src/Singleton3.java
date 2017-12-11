fe7348cb160b48e98afe71b000044e0ac75f4ffd
Daniel Solawa <danielsolawa@gmail.com>
2017-12-11 11:50:07 +0100

    added third singleton implementation

---
+++ src/Singleton3.java
@@ -0,0 +1,23 @@
+/**
+ * This implementation is thread safe
+ */
+public class Singleton3 {
+
+    private static volatile Singleton3 instance = null;
+
+    private Singleton3(){
+
+    }
+
+    public static Singleton3 getInstance(){
+        if(instance == null){
+            synchronized (Singleton3.class){
+                if(instance == null){
+                    instance = new Singleton3();
+                }
+            }
+        }
+
+        return instance;
+    }
+}
